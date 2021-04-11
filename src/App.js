f3803f93dc3aed70fd780f147388e6b73459f1a3
nvthuong6998 <73069583+nvthuong6998@users.noreply.github.com>
2021-04-11 11:19:44 +0700

    nvthuong

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
+import React from 'react';
+import data from './data.json';
+import Products from '../src/components/Products'
+
+class App extends React.Component {
+
+  constructor(props) {
+    super();
+    this.state = {
+      products: data.products,
+      size:"",
+      sort:"",
+    }
+  }
+
+  render(){
+    return (
+      <div className="grid-container">
+
+      {/* -- Header -- */}
+      
+        <header>
+          <a href="/">React shopping cart </a>
+        </header>
+
+      {/* ---- Body ---- */}
+
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products} />
+            </div>
+            <div className="sidebar">
+              Cart Items
+            </div>
+          </div>
+        </main>
 
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">React shopping cart </a>
-      </header>
-      <main>Product list</main>
-      <footer>Nguyen Van Thuong</footer>
-    </div>
-  );
+
+        {/* ---- Footer ---- */}
+        <footer>Nguyen Van Thuong</footer>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
